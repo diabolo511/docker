bfd806d175a432e3b7332a58a048dc56b3caf569
Dennis <93436829+diabolo511@users.noreply.github.com>
2025-01-11 12:31:21 +0100

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-#install docker and portainer
+#install docker and portainer in one script
+#first delete old packages related
 
 for pkg in docker.io docker-doc docker-compose podman-docker containerd runc; do sudo apt-get remove $pkg; done
+
 # Add Docker's official GPG key:
+
 sudo apt-get update
 sudo apt-get install ca-certificates curl
 sudo install -m 0755 -d /etc/apt/keyrings
@@ -15,12 +18,17 @@ echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
 sudo apt-get update
 
 # install Docker
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+# Test if Docker works 
 sudo docker run hello-world
 
-#install Portainer with Port 9443
+# create Data Volume for portainer
 docker volume create portainer_data
+
+#install portainer on port 9443 with self-signed SSL cert
 docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce
